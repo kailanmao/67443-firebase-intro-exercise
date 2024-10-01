b539f184f603029e07513fff475a4a212906b6df
Kailan Mao <kailanm@andrew.cmu.edu>
2024-10-01 18:58:40 -0400

    done

---
+++ Pods/Target Support Files/Pods-PriceCheckFB/Pods-PriceCheckFB-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PriceCheckFB : NSObject
+@end
+@implementation PodsDummy_Pods_PriceCheckFB
+@end
---
+++ PriceCheckFB/PriceCheckFBApp.swift
@@ -0,0 +1,31 @@
+// Created by Prof. H in 2022
+// Part of the PriceCheckFB project
+// Using Swift 5.0
+// Qapla'
+
+import SwiftUI
+// import Firebase modules here
+import FirebaseCore
+import FirebaseFirestore
+
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
+
+@main
+struct PriceCheckFBApp: App {
+  // register app delegate for Firebase setup
+  @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
+  var body: some Scene {
+    WindowGroup {
+      AppView()
+    }
+  }
+}
